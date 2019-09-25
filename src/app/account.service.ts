9cad7602e77c758c89983252b42aa04c37263a44
mybene <benoite251@gmial.com>
2019-09-25 09:30:56 +0200

    create acoount service

---
+++ src/app/account.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Repository } from './repository';
+import {User} from './user';
+import { Account } from './account';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor() { }
+}
