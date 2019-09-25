732b138679e16b5caa3fc1371250b0023e97c813
mybene <benoite251@gmial.com>
2019-09-25 09:36:40 +0200

    create date count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+// import { AccountService } from './account.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
 import { AccountComponent } from './account/account.component';
+import { InputComponent } from './input/input.component';
+import { HighligthedDirective } from './highligthed.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AccountComponent,
     InputComponent,
-    AccountComponent
+    // AccountService,
+    HighligthedDirective,
+    DateCountPipe 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
